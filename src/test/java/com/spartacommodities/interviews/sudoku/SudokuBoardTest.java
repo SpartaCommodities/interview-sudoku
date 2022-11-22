6b6604a48145f6038b1f547b6fb51de803242d17
paco <paco.estevez@spartacommodities.com>
2022-11-22 11:04:46 +0000

    feat: make tests for failed row, column, and square

--- src/test/java/com/spartacommodities/interviews/sudoku/SudokuBoardTest.java
+++ src/test/java/com/spartacommodities/interviews/sudoku/SudokuBoardTest.java
@@ -78,4 +78,38 @@ class SudokuBoardTest {
 
         assertFalse(new SudokuBoard(invalidBoard).isSolved(), "An incorrect board should not be solved");
     }
+
+    @Test
+    void invalidBoardWithIncorrectRowIsNotSolved() {
+        final int[][] invalidBoard = new int[][]{
+                {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                {2, 8, 7, 4, 1, 9, 6, 3, 9 /* wrong */},
+                {3, 4, 5, 2, 8, 6, 1, 7, 5 /* wrong */},
+        };
+
+        assertFalse(new SudokuBoard(invalidBoard).isSolved(), "An incorrect board should not be solved");
+    }
+
+    @Test
+    void invalidBoardWithIncorrectSquareIsNotSolved() {
+        final int[][] invalidBoard = new int[][]{
+                {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {3, 4, 5, 2, 8, 6, 1, 7, 5}, /* wrong, should be last row */
+                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                {2, 8, 7, 4, 1, 9, 6, 3, 9},
+        };
+
+        assertFalse(new SudokuBoard(invalidBoard).isSolved(), "An incorrect board should not be solved");
+    }
 }

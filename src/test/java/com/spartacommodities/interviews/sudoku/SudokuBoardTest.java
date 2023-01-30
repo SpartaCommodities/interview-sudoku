2da11da91ec1a5c55e5d26fa1f3c57e3ed08a156
paco <paco.estevez@spartacommodities.com>
2023-01-30 15:57:34 +0000

    fix: correct test last, better test descriptions

--- src/test/java/com/spartacommodities/interviews/sudoku/SudokuBoardTest.java
+++ src/test/java/com/spartacommodities/interviews/sudoku/SudokuBoardTest.java
@@ -11,23 +11,6 @@ class SudokuBoardTest {
     }
 
     @Test
-    void fullBoardIsSolved() {
-        final int[][] solvedBoard = new int[][]{
-                {5, 3, 4, 6, 7, 8, 9, 1, 2},
-                {6, 7, 2, 1, 9, 5, 3, 4, 8},
-                {1, 9, 8, 3, 4, 2, 5, 6, 7},
-                {8, 5, 9, 7, 6, 1, 4, 2, 3},
-                {4, 2, 6, 8, 5, 3, 7, 9, 1},
-                {7, 1, 3, 9, 2, 4, 8, 5, 6},
-                {9, 6, 1, 5, 3, 7, 2, 8, 4},
-                {2, 8, 7, 4, 1, 9, 6, 3, 5},
-                {3, 4, 5, 2, 8, 6, 1, 7, 9},
-        };
-
-        assertTrue(new SudokuBoard(solvedBoard).isSolved(), "A full correct board should be solved");
-    }
-
-    @Test
     void boardWithGapsIsNotSolved() {
         final int[][] boardWithEmptyCells = new int[][]{
                 {5, 0, 4, 6, 7, 8, 9, 1, 2},
@@ -41,7 +24,7 @@ class SudokuBoardTest {
                 {3, 4, 5, 2, 0, 6, 1, 7, 9},
         };
 
-        assertFalse(new SudokuBoard(boardWithEmptyCells).isSolved(), "A board with empty cells should not be solved");
+        assertFalse(new SudokuBoard(boardWithEmptyCells).isSolved(), "A board with wrong cells should not be solved");
     }
 
     @Test
@@ -76,7 +59,7 @@ class SudokuBoardTest {
                 {3, 4, 5, 2, 8, 6, 1, 9 /* wrong */, 7 /* wrong */},
         };
 
-        assertFalse(new SudokuBoard(invalidBoard).isSolved(), "An incorrect board should not be solved");
+        assertFalse(new SudokuBoard(invalidBoard).isSolved(), "A board with an incorrect COLUMN should not be solved");
     }
 
     @Test
@@ -93,7 +76,7 @@ class SudokuBoardTest {
                 {3, 4, 5, 2, 8, 6, 1, 7, 5 /* wrong */},
         };
 
-        assertFalse(new SudokuBoard(invalidBoard).isSolved(), "An incorrect board should not be solved");
+        assertFalse(new SudokuBoard(invalidBoard).isSolved(), "A board with an incorrect ROW should not be solved");
     }
 
     @Test
@@ -110,6 +93,23 @@ class SudokuBoardTest {
                 {2, 8, 7, 4, 1, 9, 6, 3, 9},
         };
 
-        assertFalse(new SudokuBoard(invalidBoard).isSolved(), "An incorrect board should not be solved");
+        assertFalse(new SudokuBoard(invalidBoard).isSolved(), "A board with an incorrect SQUARE should not be solved");
+    }
+
+    @Test
+    void fullBoardIsSolved() {
+        final int[][] solvedBoard = new int[][]{
+                {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                {3, 4, 5, 2, 8, 6, 1, 7, 9},
+        };
+
+        assertTrue(new SudokuBoard(solvedBoard).isSolved(), "A full correct board should be solved");
     }
 }
